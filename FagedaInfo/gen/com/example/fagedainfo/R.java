f517bfccaa2309319eaf5cbec62a036e6432ac11
Dani <Dani@infobosccoma.net>
2014-03-27 17:16:31 +0100

    ok

--- FagedaInfo/gen/com/example/fagedainfo/R.java
+++ FagedaInfo/gen/com/example/fagedainfo/R.java
@@ -23,14 +23,16 @@ public final class R {
     public static final class drawable {
         public static final int foto=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int logo_vaca=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080006;
-        public static final int button2=0x7f080001;
+        public static final int LinearLayout2=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int buttonFoto=0x7f080007;
         public static final int editText1=0x7f080003;
         public static final int editText2=0x7f080005;
+        public static final int imageView1=0x7f080001;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080004;
     }
